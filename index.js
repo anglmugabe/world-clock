6bb274fee15359f37cc2a2d39e87cc4c0c5ddd74
anglmugabe <anglmugabe@gmail.com>
2024-12-10 13:19:54 +0200

    updated my two locations js

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-let kinshasaElement = document.querySelector("#kinshasa");
+setInterval(function () {
+	let kinshasaElement = document.querySelector("#kinshasa");
 
-let kinshasaDateElement = document.querySelector("#kinshasa.date");
+	let kinshasaDateElement = kinshasaElement.querySelector(".date");
+	let kinshasaTimeElement = kinshasaElement.querySelector(".time");
+	let kinshasaTime = moment().tz("Africa/Kinshasa");
 
-kinshasaElement.innerHTML = "";
+	kinshasaDateElement.innerHTML = moment().format("MMMM Do YYYY");
+	kinshasaTimeElement.innerHTML = kinshasaTime.format(
+		"h:mm:ss [<small>]A[</small>]"
+	);
+}, 1000);
+
+setInterval(function () {
+	let cairoElement = document.querySelector("#cairo");
+
+	let cairoDateElement = cairoElement.querySelector(".date");
+	let cairoTimeElement = cairoElement.querySelector(".time");
+	let cairoTime = moment().tz("Africa/Cairo");
+
+	cairoDateElement.innerHTML = moment().format("MMMM Do YYYY");
+	cairoTimeElement.innerHTML = cairoTime.format("h:mm:ss [<small>]A[</small>]");
+}, 1000);
